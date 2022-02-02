76ae30ffdea9b2d721691664bcb9437492381fbd
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-02 10:57:39 -0800

    add second test for Haiku class with code for good fail
    Co-authored-by: S-Espinet <s.lou.espinet@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,12 +1,15 @@
-import { Haiku } from './../src/js/haiku.js';
+import Haiku from './../src/js/haiku.js';
 
 describe('Haiku', () => {
-
   test('should correctly create a haiku object with three lines', () => {
-    const myHaiku = new Haiku("Delightful display", "Snowdrops bow their pure white heads", "To the sun's glory")
+    let myHaiku = new Haiku("Delightful display", "Snowdrops bow their pure white heads", "To the sun's glory");
     expect(myHaiku.line1).toEqual("Delightful display");
     expect(myHaiku.line2).toEqual("Snowdrops bow their pure white heads");
     expect(myHaiku.line3).toEqual("To the sun's glory");
   });
-});
 
+  test('should correctly count vowels in a line', () => {
+    let myHaiku = new Haiku ("Delightful display", "Snowdrops bow their pure white heads", "To the sun's glory");
+    expect(myHaiku.findSyllable()).toEqual(5)
+  });
+});
