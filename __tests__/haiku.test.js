72856187c824662048041474ff368f102c8349d1
S. Espinet <s.lou.espinet@gmail.com>
2022-02-03 09:27:41 -0800

    add silent e test with silent e word - good fail
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -7,8 +7,12 @@ describe('Haiku', () => {
     expect(myHaiku.line2).toEqual("Snowdrops bow their pure white heads");
     expect(myHaiku.line3).toEqual("To the sun's glory");
   });
-  test('should not include the silent e at the end of a word', () => {
+  test('should return word if it does not end with e', () => {
     let myHaiku = new Haiku ("Delightful");
     expect(myHaiku.removeE()).toEqual("Delightful")
+  });
+  test('should not include the silent e at the end of a word', () => {
+    let myHaiku = new Haiku ("white");
+    expect(myHaiku.removeE()).toEqual("whit")
   });
 });
